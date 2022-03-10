1ee5cef741760909071b1db5545fcebdd3e57a1d
samxu01 <xcjsam@gmail.com>
2022-03-09 22:04:02 -0500

    changed positions

--- walking-gold-miner.js
+++ walking-gold-miner.js
@@ -19,8 +19,8 @@ export class WalkingGoldMiner extends Scene {
         this.position=0;
         this.light=false;
         this.time=0;
-        this.x_list=[4, 0, 12, -3 , -6, -7];
-        this.y_list=[-3, 0, -3, -7, -8, -5];
+        this.x_list=[4, 1, 12, -3 , -10, -6];
+        this.y_list=[-3, 0, -3, -7, -2, -4];
         this.collide=false;
         this.collide_x=-99;
         this.collide_y=-99;
@@ -31,10 +31,6 @@ export class WalkingGoldMiner extends Scene {
             circle: new defs.Regular_2D_Polygon(1, 15),
             sun: new defs.Subdivision_Sphere(4),
             planet_1: new (defs.Subdivision_Sphere.prototype.make_flat_shaded_version())(1.5),
-            planet_2: new defs.Subdivision_Sphere(4),
-            planet_3: new defs.Subdivision_Sphere(4),
-            planet_4: new defs.Subdivision_Sphere(4),
-            moon: new (defs.Subdivision_Sphere.prototype.make_flat_shaded_version())(1),
             backwall: new defs.Cube,
             skyscraper: new defs.Cube,
             flashbang: new defs.Cylindrical_Tube(3,15, [[0, 1], [0, 1]]),
@@ -44,13 +40,6 @@ export class WalkingGoldMiner extends Scene {
 
         // *** Materials
         this.materials = {
-            test: new Material(new defs.Phong_Shader(),
-                {ambient: .4, diffusivity: .6, color: hex_color("#ffffff")}),
-            test2: new Material(new Gouraud_Shader(),
-                {ambient: .4, diffusivity: .6, color: hex_color("#992828")}),
-            ring: new Material(new Ring_Shader()),
-            // TODO:  Fill in as many additional material objects as needed in this key/value table.
-            //        (Requirement 4)
             sun: new Material(new defs.Phong_Shader(),
                 {ambient: 1, color: color(1, 1, 1, 1)}),
             sun1: new Material(new defs.Phong_Shader(),
@@ -59,18 +48,6 @@ export class WalkingGoldMiner extends Scene {
                 {ambient: 0, color: hex_color("#FFD700")}),
             stone: new Material(new defs.Phong_Shader(),
                 {ambient: 0, color: hex_color("#a8a3a3")}),
-            planet_2: new Material(new defs.Phong_Shader(),
-                {ambient: 1, diffusivity: .2, specular: 1, color: hex_color("#80FFFF")}),
-            planet_2_g: new Material(new Gouraud_Shader(),
-                {ambient: 0, diffusivity: .2, specular: 1, color: hex_color("#80FFFF")}),
-            planet_3: new Material(new defs.Phong_Shader(),
-                {ambient: 0, diffusivity: 1, specular: 1, color: hex_color("#B08040")}),
-            planet_3_ring: new Material(new Ring_Shader(),
-                {color: hex_color("#B08040")}),
-            planet_4: new Material(new defs.Phong_Shader(),
-                {ambient: 0, diffusivity: 1, specular: 1, color: hex_color("#C7E4EE")}),
-            moon: new Material(new defs.Phong_Shader(),
-                {ambient: 0, diffusivity: 1, specular: 1, color: hex_color("#FA8072")}),
             skyscraper: new Material(new defs.Textured_Phong(1),
                 {ambient: 1, diffusivity: 1, specular: 0.5, texture: new Texture("assets/skyscraper.jpg")}),
             sky: new Material(new defs.Textured_Phong(1),
@@ -231,7 +208,7 @@ export class WalkingGoldMiner extends Scene {
 
         let stone2_tr = Mat4.identity();
         stone2_tr = stone2_tr.times(Mat4.translation(this.x_list[4],this.y_list[4],-1))
-        if (this.collide_x === -6 && this.collide_y === -8)
+        if (this.collide_x === -10 && this.collide_y === -2)
         {
             stone2_tr = this.hookTr;
             stone2_tr = stone2_tr.times(Mat4.translation(0,-2,0)).times(Mat4.inverse(Mat4.scale(0.4, 0.4, 0.4)));
@@ -240,7 +217,7 @@ export class WalkingGoldMiner extends Scene {
 
         let stone3_tr = Mat4.identity();
         stone3_tr = stone3_tr.times(Mat4.translation(this.x_list[5],this.y_list[5],-1))
-        if (this.collide_x === -7 && this.collide_y === -5)
+        if (this.collide_x === -6 && this.collide_y === -4)
         {
             stone3_tr = this.hookTr;
             stone3_tr = stone3_tr.times(Mat4.translation(0,-2,0)).times(Mat4.inverse(Mat4.scale(0.4, 0.4, 0.4)));
@@ -250,7 +227,7 @@ export class WalkingGoldMiner extends Scene {
         //draw gold
         let gold_tr= Mat4.identity();
         gold_tr= gold_tr.times(Mat4.translation(this.x_list[1],this.y_list[1],-1))
-        if (this.collide_x === 0 && this.collide_y === 0)
+        if (this.collide_x === 1 && this.collide_y === 0)
         {
             gold_tr = this.hookTr;
             gold_tr = gold_tr.times(Mat4.translation(0,-2,0)).times(Mat4.inverse(Mat4.scale(0.4, 0.4, 0.4)));
@@ -384,226 +361,6 @@ export class WalkingGoldMiner extends Scene {
         }
 
         this.shapes.hook.draw(context, program_state, hook_tr, this.materials.hook);
-
-/*        let test_tr= Mat4.identity();
-        test_tr= test_tr.times(Mat4.translation(3,0,0))
-        this.shapes.planet_1.draw(context, program_state, test_tr, this.materials.hook)
-
-        let test2_tr= Mat4.identity();
-        test2_tr= test2_tr
-            .times(Mat4.scale(0.4,0.4,0.4))
-            .times(Mat4.translation(3/0.4,0,12))
-
-        this.shapes.planet_1.draw(context, program_state, test2_tr, this.materials.gold)*/
-
-        /*if (this.attached) {
-            if (this.attached() == null)
-            {
-                let desired = this.initial_camera_location.map( (x,i) =>
-                    Vector.from( program_state.camera_inverse[i] ).mix( x, 0.1 ) );
-                program_state.set_camera(desired);
-            }
-            else
-            {
-                let desired = Mat4.inverse(this.attached().times(Mat4.translation(0, 0, 5)));
-                desired = desired.map( (x,i) =>
-                    Vector.from( program_state.camera_inverse[i] ).mix( x, 0.1 ) );
-                program_state.set_camera(desired);
-            }
-        }*/
-        // TODO:  Fill in matrix operations and drawing code to draw the solar system scene (Requirements 3 and 4)
-    }
-}
-
-class Gouraud_Shader extends Shader {
-    // This is a Shader using Phong_Shader as template
-    // TODO: Modify the glsl coder here to create a Gouraud Shader (Planet 2)
-
-    constructor(num_lights = 2) {
-        super();
-        this.num_lights = num_lights;
-    }
-
-    shared_glsl_code() {
-        // ********* SHARED CODE, INCLUDED IN BOTH SHADERS *********
-        return ` 
-        precision mediump float;
-        const int N_LIGHTS = ` + this.num_lights + `;
-        uniform float ambient, diffusivity, specularity, smoothness;
-        uniform bool gouraud;
-        uniform vec4 light_positions_or_vectors[N_LIGHTS], light_colors[N_LIGHTS];
-        uniform float light_attenuation_factors[N_LIGHTS];
-        uniform vec4 shape_color;
-        uniform vec3 squared_scale, camera_center;
-
-        // Specifier "varying" means a variable's final value will be passed from the vertex shader
-        // on to the next phase (fragment shader), then interpolated per-fragment, weighted by the
-        // pixel fragment's proximity to each of the 3 vertices (barycentric interpolation).
-        varying vec3 N, vertex_worldspace;
-        varying vec3 vertex_color;
-        // ***** PHONG SHADING HAPPENS HERE: *****
-                                          
-        vec3 phong_model_lights( vec3 N, vec3 vertex_worldspace ){                                        
-            // phong_model_lights():  Add up the lights' contributions.
-            vec3 E = normalize( camera_center - vertex_worldspace );
-            vec3 result = vec3( 0.0 );
-            for(int i = 0; i < N_LIGHTS; i++){
-                // Lights store homogeneous coords - either a position or vector.  If w is 0, the 
-                // light will appear directional (uniform direction from all points), and we 
-                // simply obtain a vector towards the light by directly using the stored value.
-                // Otherwise if w is 1 it will appear as a point light -- compute the vector to 
-                // the point light's location from the current surface point.  In either case, 
-                // fade (attenuate) the light as the vector needed to reach it gets longer.  
-                vec3 surface_to_light_vector = light_positions_or_vectors[i].xyz - 
-                                               light_positions_or_vectors[i].w * vertex_worldspace;                                             
-                float distance_to_light = length( surface_to_light_vector );
-
-                vec3 L = normalize( surface_to_light_vector );
-                vec3 H = normalize( L + E );
-                // Compute the diffuse and specular components from the Phong
-                // Reflection Model, using Blinn's "halfway vector" method:
-                float diffuse  =      max( dot( N, L ), 0.0 );
-                float specular = pow( max( dot( N, H ), 0.0 ), smoothness );
-                float attenuation = 1.0 / (1.0 + light_attenuation_factors[i] * distance_to_light * distance_to_light );
-                
-                vec3 light_contribution = shape_color.xyz * light_colors[i].xyz * diffusivity * diffuse
-                                                          + light_colors[i].xyz * specularity * specular;
-                result += attenuation * light_contribution;
-            }
-            return result;
-        } `;
-    }
-
-    vertex_glsl_code() {
-        // ********* VERTEX SHADER *********
-        return this.shared_glsl_code() + `
-            attribute vec3 position, normal;                            
-            // Position is expressed in object coordinates.
-            
-            uniform mat4 model_transform;
-            uniform mat4 projection_camera_model_transform;
-    
-            void main(){                                                                   
-                // The vertex's final resting place (in NDCS):
-                gl_Position = projection_camera_model_transform * vec4( position, 1.0 );
-                // The final normal vector in screen space.
-                N = normalize( mat3( model_transform ) * normal / squared_scale);
-                vertex_worldspace = ( model_transform * vec4( position, 1.0 ) ).xyz;
-                vertex_color = phong_model_lights(N, vertex_worldspace);
-            } `;
-    }
-
-    fragment_glsl_code() {
-        // ********* FRAGMENT SHADER *********
-        // A fragment is a pixel that's overlapped by the current triangle.
-        // Fragments affect the final image or get discarded due to depth.
-        return this.shared_glsl_code() + `
-            void main(){                                                           
-                gl_FragColor = vec4( vertex_color.xyz, 1.0 );
-            } `;
-    }
-
-    send_material(gl, gpu, material) {
-        // send_material(): Send the desired shape-wide material qualities to the
-        // graphics card, where they will tweak the Phong lighting formula.
-        gl.uniform4fv(gpu.shape_color, material.color);
-        gl.uniform1f(gpu.ambient, material.ambient);
-        gl.uniform1f(gpu.diffusivity, material.diffusivity);
-        gl.uniform1f(gpu.specularity, material.specularity);
-        gl.uniform1f(gpu.smoothness, material.smoothness);
-    }
-
-    send_gpu_state(gl, gpu, gpu_state, model_transform) {
-        // send_gpu_state():  Send the state of our whole drawing context to the GPU.
-        const O = vec4(0, 0, 0, 1), camera_center = gpu_state.camera_transform.times(O).to3();
-        gl.uniform3fv(gpu.camera_center, camera_center);
-        // Use the squared scale trick from "Eric's blog" instead of inverse transpose matrix:
-        const squared_scale = model_transform.reduce(
-            (acc, r) => {
-                return acc.plus(vec4(...r).times_pairwise(r))
-            }, vec4(0, 0, 0, 0)).to3();
-        gl.uniform3fv(gpu.squared_scale, squared_scale);
-        // Send the current matrices to the shader.  Go ahead and pre-compute
-        // the products we'll need of the of the three special matrices and just
-        // cache and send those.  They will be the same throughout this draw
-        // call, and thus across each instance of the vertex shader.
-        // Transpose them since the GPU expects matrices as column-major arrays.
-        const PCM = gpu_state.projection_transform.times(gpu_state.camera_inverse).times(model_transform);
-        gl.uniformMatrix4fv(gpu.model_transform, false, Matrix.flatten_2D_to_1D(model_transform.transposed()));
-        gl.uniformMatrix4fv(gpu.projection_camera_model_transform, false, Matrix.flatten_2D_to_1D(PCM.transposed()));
-
-        // Omitting lights will show only the material color, scaled by the ambient term:
-        if (!gpu_state.lights.length)
-            return;
-
-        const light_positions_flattened = [], light_colors_flattened = [];
-        for (let i = 0; i < 4 * gpu_state.lights.length; i++) {
-            light_positions_flattened.push(gpu_state.lights[Math.floor(i / 4)].position[i % 4]);
-            light_colors_flattened.push(gpu_state.lights[Math.floor(i / 4)].color[i % 4]);
-        }
-        gl.uniform4fv(gpu.light_positions_or_vectors, light_positions_flattened);
-        gl.uniform4fv(gpu.light_colors, light_colors_flattened);
-        gl.uniform1fv(gpu.light_attenuation_factors, gpu_state.lights.map(l => l.attenuation));
-    }
-
-    update_GPU(context, gpu_addresses, gpu_state, model_transform, material) {
-        // update_GPU(): Define how to synchronize our JavaScript's variables to the GPU's.  This is where the shader
-        // recieves ALL of its inputs.  Every value the GPU wants is divided into two categories:  Values that belong
-        // to individual objects being drawn (which we call "Material") and values belonging to the whole scene or
-        // program (which we call the "Program_State").  Send both a material and a program state to the shaders
-        // within this function, one data field at a time, to fully initialize the shader for a draw.
-
-        // Fill in any missing fields in the Material object with custom defaults for this shader:
-        const defaults = {color: color(0, 0, 0, 1), ambient: 0, diffusivity: 1, specularity: 1, smoothness: 40};
-        material = Object.assign({}, defaults, material);
-
-        this.send_material(context, gpu_addresses, material);
-        this.send_gpu_state(context, gpu_addresses, gpu_state, model_transform);
-    }
-}
-
-class Ring_Shader extends Shader {
-    update_GPU(context, gpu_addresses, graphics_state, model_transform, material) {
-        // update_GPU():  Defining how to synchronize our JavaScript's variables to the GPU's:
-        const [P, C, M] = [graphics_state.projection_transform, graphics_state.camera_inverse, model_transform],
-            PCM = P.times(C).times(M);
-        context.uniformMatrix4fv(gpu_addresses.model_transform, false, Matrix.flatten_2D_to_1D(model_transform.transposed()));
-        context.uniformMatrix4fv(gpu_addresses.projection_camera_model_transform, false,
-            Matrix.flatten_2D_to_1D(PCM.transposed()));
-    }
-
-    shared_glsl_code() {
-        // ********* SHARED CODE, INCLUDED IN BOTH SHADERS *********
-        return `
-        precision mediump float;
-        varying vec4 point_position;
-        varying vec4 center;
-        `;
-    }
-
-    vertex_glsl_code() {
-        // ********* VERTEX SHADER *********
-        // TODO:  Complete the main function of the vertex shader (Extra Credit Part II).
-        return this.shared_glsl_code() + `
-        attribute vec3 position;
-        uniform mat4 model_transform;
-        uniform mat4 projection_camera_model_transform;
-        
-        void main(){
-            gl_Position = projection_camera_model_transform * vec4( position, 1.0 );
-            point_position = model_transform * vec4( position, 1.0 );
-            center = model_transform * vec4( 0.0, 0.0, 0.0, 1.0 );
-        }`;
-    }
-
-    fragment_glsl_code() {
-        // ********* FRAGMENT SHADER *********
-        // TODO:  Complete the main function of the fragment shader (Extra Credit Part II).
-        return this.shared_glsl_code() + `
-        void main(){
-            vec3 distance = vec3(point_position.xyz - center.xyz);
-            gl_FragColor = vec4( vec3(.69, .502, .251), cos( length(distance) * 20.0 ));
-        }`;
     }
 }
 
